173d4f5ca27bf49eff8961e72e5f11e54f9762e0
liangxiaowei <xiaoweiliang@caixin.com>
2019-01-29 14:13:49 +0800

    first commit

---
+++ mapreduce-test-java/grep/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+base_dir=$(cd "$(dirname "$0")";pwd)
+../../start.sh
+mvn clean package
+/usr/local/hadoop/bin/hdfs dfs -rm -r /grep/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /grep/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /grep/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/test.txt /grep/input/
+/usr/local/hadoop/bin/hadoop jar target/mapreduce-test-grep-1.0-SNAPSHOT.jar hadoop.mapreduce.test.grep.RunJob world
+/usr/local/hadoop/bin/hdfs dfs -cat /grep/output/part-r-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /grep/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /grep/output/
+mvn clean
+../../stop.sh
